ad10dc5cfeecf46d8900a8c60f0e10b30c1c5272
Bezedache <bezedache29@gmail.com>
2026-04-27 18:54:21 +0200

    chore: initial setup (vite, react, ts, tailwind, eslint, prettier)

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center p-4">
+      <h1 className="text-5xl font-bold text-blue-500 mb-4">
+        Christophe Salou
+      </h1>
+      <p className="text-xl text-gray-400">Développeur Full-Stack</p>
+    </div>
+  );
+}
+
+export default App;
